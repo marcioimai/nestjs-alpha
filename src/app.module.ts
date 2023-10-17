25b1bbf1b1aa4b56c7fe40369fd412818da8f165
Marcio Imai <marcioimai@gmail.com>
2023-10-17 16:38:38 -0400

    prisma + sqlite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { UserController } from './user/user.controller';
+import { PostController } from './post/post.controller';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [UserController, PostController],
+  providers: [PrismaService, UserService, PostService],
 })
 export class AppModule {}
